73f65f9af7b9193414349bc9b2b1f2bf558f04c8
Sailesh Patel <saileshapatel@gmail.com>
2015-10-28 09:09:31 +0000

    refactored code for Python 3.4

--- ascii.py
+++ ascii.py
@@ -4,16 +4,16 @@ import os
 def clean_file_input():
 	while True:
 		ext_list = ["jpg", "jpeg", "png", "bmp"]
-		name = raw_input("File Name > ")
+		name = input("File Name > ")
 		try:
 			ext = name.split('.')[1]
 		except:
-			print "File needs extension"
+			print("File needs extension")
 			return False
 		if name in os.listdir(".") and ext in ext_list:
 			return name
 		else:
-			print "Invalid filename"
+			print("Invalid filename")
 
 def to_ascii(im_name, size):
 	out_list = []
@@ -24,39 +24,39 @@ def to_ascii(im_name, size):
 	conv = imag.convert("RGB")
 	pix = conv.load()
 	width, height = conv.size
-	for Y in xrange(0, height):
-		list = []
-		for X in xrange(0, width):
+	for Y in list(range(0, height)):
+		appendList = []
+		for X in list(range(0, width)):
 			R, G, B = pix[X, Y]
 			avg = (R + G + B)/3
 			if avg < 60:
-				list.append(0)
+				appendList.append(0)
 			elif avg < 120:
-				list.append(1)
+				appendList.append(1)
 			elif avg < 180:
-				list.append(2)
+				appendList.append(2)
 			else:
-				list.append(3)
-		out_list.append(list)
+				appendList.append(3)
+		out_list.append(appendList)
 	return out_list
 
 def print_ascii(out_list):
 	for i in out_list:
 		for j in i:
 			if j == 3:
-				print " ",
+				print(" ")
 			elif j == 2:
-				print ".",
+				print(".")
 			elif j == 1:
-				print "%",
+				print("%")
 			elif j == 0:
-				print "#",
+				print("#")
 		print
 
 def ascii_to_string(out_list):
 	s = ""
-	for list in out_list:
-		for item in list:
+	for appendList in out_list:
+		for item in appendList:
 			if item == 3:
 				s += " "
 			elif item == 2:
@@ -73,5 +73,3 @@ if im_name:
 	ascii = to_ascii(im_name, 160)
    
 print_ascii(ascii)
-
-
